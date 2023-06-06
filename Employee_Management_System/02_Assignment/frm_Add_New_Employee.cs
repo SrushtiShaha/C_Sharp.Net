ecdb2e66bff5e88e8bff2dd66f7c9e937059ba8a
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2023-06-06 17:43:34 +0530

    Assignment

---
+++ Employee_Management_System/02_Assignment/frm_Add_New_Employee.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Employee_Management_System
+{
+    public partial class frm_Add_New_Employee : Form
+    {
+        public frm_Add_New_Employee()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection Con = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=Employee_Management_System_DB;Integrated Security=True");
+
+        void Con_Open()
+        {
+            if (Con.State != ConnectionState.Open)
+            {
+                Con.Open();
+            }
+        }
+
+        void Con_Close()
+        {
+            if (Con.State != ConnectionState.Closed)
+            {
+                Con.Close();
+            }
+        }
+
+        void Clear_Controls()
+        {
+            tb_Name.Clear();
+            tb_MobNo.Clear();
+            dtp_DOB.Text = "31 - 12 - 2010";
+            cmb_Des.SelectedIndex = -1;
+
+            Auto_Incr();
+            tb_Name.Focus();
+        }
+
+        void Auto_Incr()
+        {
+            Con_Open();
+
+            int Cnt = 0;
+
+            SqlCommand Cmd = new SqlCommand();
+
+            Cmd.Connection = Con;
+            Cmd.CommandText = "Select Count(*) from Employee_List";
+
+            Cnt = Convert.ToInt32( Cmd.ExecuteScalar() );
+
+            Cmd.Dispose();
+
+            if( Cnt > 0 )
+            {
+                Cmd.Connection = Con;
+                Cmd.CommandText = "Select Max(Emp_Id) from Employee_List";
+
+                Cnt = Convert.ToInt32( Cmd.ExecuteScalar() );
+
+                Cnt = Cnt + 1;
+            }
+            else
+            {
+                Cnt = 101;
+            }
+
+            tb_Id.Text =  Convert.ToString( Cnt );
+
+            Con_Close();
+        }
+
+        private void frm_Add_New_Employee_Load(object sender, EventArgs e)
+        {
+            lbl_UName.Text = Common_Content.Log_Uname;
+            Clear_Controls();
+            tb_Name.Focus();
+        }
+
+        private void Only_Numeric(object sender, KeyPressEventArgs e)
+        {
+            if (!(Char.IsDigit(e.KeyChar) || (e.KeyChar == (char)Keys.Back)))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void Only_Text(object sender, KeyPressEventArgs e)
+        {
+            if (!(Char.IsLetter(e.KeyChar) || (e.KeyChar == (char)Keys.Back) || (e.KeyChar == (char)Keys.Space)))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btn_Refresh_Click(object sender, EventArgs e)
+        {
+            Clear_Controls();
+        }
+
+        private void btn_Emp_List_Click(object sender, EventArgs e)
+        {
+            frm_View_Employee_List obj = new frm_View_Employee_List();
+            obj.Show();
+            this.Hide();
+        }
+
+        private void lbl_Logout_Click(object sender, EventArgs e)
+        {
+            frm_Login obj = new frm_Login();
+            obj.Show();
+            this.Hide();
+        }
+
+        private void btn_Save_Click(object sender, EventArgs e)
+        {
+            Con_Open();
+
+            if (tb_Id.Text != "" && tb_Name.Text != "" && tb_MobNo.Text != "" && cmb_Des.Text != "")
+            {
+                SqlCommand cmd = new SqlCommand();
+
+                cmd.Connection = Con;
+                cmd.CommandText = "Insert Into Employee_List Values(@Id, @Nm, @MobNo, @DOB, @Des)";
+
+                cmd.Parameters.Add("Id", SqlDbType.Int).Value = tb_Id.Text;
+                cmd.Parameters.Add("Nm", SqlDbType.VarChar).Value = tb_Name.Text;
+                cmd.Parameters.Add("MobNo", SqlDbType.Decimal).Value = tb_MobNo.Text;
+                cmd.Parameters.Add("DOB", SqlDbType.Date).Value = dtp_DOB.Value.Date;
+                cmd.Parameters.Add("Des", SqlDbType.NVarChar).Value = cmb_Des.Text;
+
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Record Inserted Successfully", "Success", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+
+                Clear_Controls();
+            }
+            else
+            {
+                MessageBox.Show("First Fill All Fields", "Incomplete Infomation", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            Con_Close();
+        }
+    }
+}
---
+++ Employee_Management_System/02_Assignment/frm_Login.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Employee_Management_System
+{
+    public partial class frm_Login : Form
+    {
+        public frm_Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Only_Text(object sender, KeyPressEventArgs e)
+        {
+            if (!(Char.IsLetter(e.KeyChar) || (e.KeyChar == (char)Keys.Back) || (e.KeyChar == (char)Keys.Space)))
+            {
+                e.Handled = true;
+            }
+        }
+
+        SqlConnection Con = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=Employee_Management_System_DB;Integrated Security=True");
+
+        void Con_Open()
+        {
+            if( Con.State != ConnectionState.Open )
+            {
+                Con.Open();
+            }
+        }
+
+        void Con_Close()
+        {
+            if( Con.State != ConnectionState.Closed )
+            {
+                Con.Close();
+            }
+        }
+
+        private void frm_Login_Load(object sender, EventArgs e)
+        {
+            lbl_Note.Text = "Enter Valid Username And Password";
+        }
+
+        private void btn_Submit_Click(object sender, EventArgs e)
+        {
+            Con_Open();
+
+            int Cnt = 0;
+
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.Connection = Con;
+            cmd.CommandText = "Select Count(*) from Login_Details Where Username = @Unm And Password = @Pwd";
+
+            cmd.Parameters.Add("Unm", SqlDbType.VarChar).Value = tb_Username.Text;
+            cmd.Parameters.Add("Pwd", SqlDbType.NVarChar).Value = tb_Password.Text;
+
+            Cnt = Convert.ToInt32(cmd.ExecuteScalar());
+
+            if( Cnt > 0 )
+            {
+                MessageBox.Show("Login Successful", "Welcome", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                Common_Content.Log_Uname = "Welcome " + tb_Username.Text;
+
+                frm_Add_New_Employee obj = new frm_Add_New_Employee();
+                obj.Show();
+                this.Hide();
+            }
+            else 
+            {
+                lbl_Note.Text = "Incorrect Username Or Password!!!";
+                lbl_Note.ForeColor = Color.Red;
+            }
+
+            tb_Username.Clear();
+            tb_Password.Clear();
+
+            Con_Close();
+        }
+    }
+}
